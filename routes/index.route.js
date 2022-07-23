dafcc98bf8c4abd809948321ddd1f951cbd6e255
Maulana Adil <maulanaadil.latif@gmail.com>
2022-07-23 09:40:22 +0700

    fix first commit

--- routes/index.route.js
+++ routes/index.route.js
@@ -1,5 +1,4 @@
 const indexController = require('../controllers/index.controller');
-const postController = require('../controllers/post.controller');
 
 const auth = require('../middlewares/auth');
 
