7cf20c2cea47717bc2f44e035abfe089f8a52231
Aziz Riza <saitamasensei710@gmail.com>
2022-07-23 12:03:14 +0700

    list cities

--- routes/index.route.js
+++ routes/index.route.js
@@ -2,6 +2,7 @@ const indexController = require('../controllers/index.controller');
 const signInController = require('../controllers/signin.controller');
 const signUpController = require('../controllers/signup.controller');
 const articlesController = require('../controllers/articles.controller');
+const listcitiesController = require('../controllers/listcities.controller');
 
 const auth = require('../middlewares/auth');
 
@@ -11,5 +12,9 @@ router.get('/', indexController.index);
 router.get('/articles', articlesController.index);
 router.get('/signin', signInController.index);
 router.get('/signup', signInController.index);
+
+router.get('/list-cities', listcitiesController.index);
+
+
 
 module.exports = router;
