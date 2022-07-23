5141b7f0bd1b917976c26cb6e39bb76d31ca4315
Maulana Adil <maulanaadil.latif@gmail.com>
2022-07-23 10:29:42 +0700

    fix typos

--- routes/index.route.js
+++ routes/index.route.js
@@ -1,14 +1,7 @@
 const indexController = require('../controllers/index.controller');
-<<<<<<< HEAD
 const signInController = require('../controllers/signin.controller');
 const signUpController = require('../controllers/signup.controller');
 const articlesController = require('../controllers/articles.controller');
-=======
-<<<<<<< HEAD
-// const postController = require('../controllers/post.controller');
-=======
->>>>>>> dafcc98bf8c4abd809948321ddd1f951cbd6e255
->>>>>>> 26435beeb4490760f3b29d3c407da1953fbf97fe
 
 const auth = require('../middlewares/auth');
 
@@ -17,6 +10,6 @@ const router = require('express').Router();
 router.get('/', indexController.index);
 router.get('/articles', articlesController.index);
 router.get('/signin', signInController.index);
-router.get('/signup', signUpController.index);
+router.get('/signup', signInController.index);
 
 module.exports = router;
